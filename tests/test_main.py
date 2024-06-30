4ee3100e6e32f8983e8789ece93a8ebf2bf26184
Gael Ramos <esgaelramos@gmail.com>
2024-06-30 13:28:37 -0600

    tests(src) add dependencies libs and first simple unit tests

---
+++ tests/test_main.py
@@ -0,0 +1,18 @@
+"""Tests for Main Module for the FastAPI-Financial application."""
+
+from fastapi.testclient import TestClient
+from src.app import app
+
+# Create a test client for our FastAPI application
+client = TestClient(app)
+
+
+def test_hello_world():
+    """Test for the hello world endpoint of the API."""
+    expected_response = "Hello World Grimorios!"
+
+    response = client.get("/")
+
+    assert response.status_code == 200
+    assert response.json() == expected_response
+
