37864ac11248d94c4b50f14419b047663815deab
Gael Ramos <esgaelramos@gmail.com>
2024-06-30 19:57:52 -0600

    feat(src) add models request and grimorio for mapping the data to insert in db

---
+++ src/models/request_model.py
@@ -0,0 +1,29 @@
+"""Models for the User Table in the DataBase."""
+
+from sqlalchemy import Column, Integer, String, Boolean
+from core.database import Base
+
+
+class Request(Base):
+    """Model for the User Table in the DataBase."""
+
+    __tablename__ = "requests"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(20))
+    last_name = Column(String(20))
+    identification = Column(String(10))
+    age = Column(Integer)
+    magic_affinity = Column(String(20))
+    status = Column(String(20), default="Pending")
+    is_deleted = Column(Boolean, default=False)
+
+
+class Grimorio(Base):
+    """Model for the Grimorio Table in the DataBase."""
+
+    __tablename__ = "grimorios"
+
+    id = Column(Integer, primary_key=True, index=True)
+    type_trebol = Column(String(20))
+    request_id = Column(Integer)
